7cc5147ed168cafaafb49fa338f5177c8fa5aa3d
LiteEze <liteversion09@gmail.com>
2024-02-11 23:44:56 +0100

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+This module houses a class named `BaseModel`,
+which serves as the foundational structure
+for other classes within the AirBnB Clone project.
+"""
 from uuid import uuid4
 import datetime
 
 class BaseModel:
+    """ this is the Base class for other class"""
+
     def __init__(self, *args, **kwargs):
+        """
+        initialize a value
+        """
+
         if kwargs != {}:
             for key in kwargs.keys():
                 if key == "__class__":
@@ -17,12 +28,23 @@ class BaseModel:
         self.updated_at = datetime.datetime.today()
 
     def __str__(self):
+        """
+        Return the print/str representation of the BaseModel instance.
+        """
+
         return("[{}] ({}) {}".format(type(self).__name__,self.id, self.__dict__))
 
     def save(self):
+
+        """updated_at with the current datetime.
+        """
+
         self.updated_at = datetime.datetime.today()
 
     def to_dict(self):
+        """Return dictionary of the Basemodel
+        """
+
         updated_at_str = self.__dict__["updated_at"].isoformat()
         created_at_str = self.__dict__["created_at"].isoformat()
         obj_dict = self.__dict__.copy()

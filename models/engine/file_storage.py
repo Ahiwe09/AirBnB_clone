e892c7c438eba39a4888e762f8c3cb83e5329730
LiteEze <liteversion09@gmail.com>
2024-02-13 16:59:55 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,11 +2,11 @@
 """Defines the FileStorage class."""
 import json
 from models.user import User
-"""from models.state import State"""
-"""from models.place import Place"""
-"""from models.amenity import Amenity"""
-"""from models.review import Review"""
-"""from models.city import City"""
+from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models.city import City
 from models.base_model import BaseModel
 
 
